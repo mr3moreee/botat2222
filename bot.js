cb996af00dc2b1f96508dcfdb3554a476c951e6c
Mr_3moreee <45393804+mr3moreee@users.noreply.github.com>
2019-01-15 12:45:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,7 @@
-client.on("message", msg => {
-  var prefix = '!'//البركفس
-  if(msg.content.startsWith(prefix + 'id')){
-    let embed = new Discord.RichEmbed()
-    .setThumbnail(msg.author.displayAvatarURL)
-    .setColor("RANDOM")
-    .addField("Year📆",msg.author.createdAt.getFullYear())
-    .addField("Hour📆", msg.author.createdAt.getHours())
-    .addField("Day📆", msg.author.createdAt.getDay())
-    .addField("Month📆", msg.author.createdAt.getMonth())
-    .addField("Minutes📆", msg.author.createdAt.getMinutes())
-    .addField("Seconds📆", msg.author.createdAt.getSeconds())
-    .addField("Full📆", msg.author.createdAt.toLocaleString())
-    .setTimestamp()
-    msg.channel.send(embed);
-  }
+const bot = require('discord.js-musicbot-addon');
+
+bot.start(client, {
+  youtubeKey: "AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8",
+  musichelp : ".help",
+ botPrefix : "."
 });
